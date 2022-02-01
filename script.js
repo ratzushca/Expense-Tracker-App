d928d84d2668f59f584097c1c5d2f911b1258565
ratzushca <93014061+ratzushca@users.noreply.github.com>
2022-01-31 17:58:03 -0800

    delete button added

--- script.js
+++ script.js
@@ -5,24 +5,25 @@ addBtn.addEventListener('click',addItem);
 function addItem(e){
     e.preventDefault();
 
+   
+
     const table = document.getElementById("table");
-    const row = document.createElement("tr");
+    const row = document.createElement('tr');
     console.log(row);
 
-  
+   
 
 
 
 
+
+    
 
     const tdtype= document.createElement("td")
     const tddata= document.createElement("td")
     const tddescription= document.createElement("td")
     const tdamount= document.createElement("td")
     const tdlocation= document.createElement("td")
-
-
-  
 
     tdtype.innerHTML=document.getElementById("types").value;
     tddata.innerHTML=document.getElementById("dates").value;
@@ -34,25 +35,24 @@ function addItem(e){
     row.appendChild(tddata);
     row.appendChild(tddescription);
     
-    
-
-
     const dollar = document.createElement("span");
     dollar.textContent = "$";
     tdamount.append(dollar);
     row.appendChild(tdamount);
-
-
-    // const text = document.getElementById("howmuch");
-    // row.appendChild(document.createTextNode(text.value));
-    // row.appendChild(dollar);
-    // table.appendChild(row);
-
-    
-
     row.appendChild(tdlocation);
 
+// delete button
+
+    const x = document.createElement('button');
+    x.textContent = "X";
+    const rows = document.getElementById("table");
+    // row.appendChild(document.createTextNode(rows.value));
+    row.appendChild(x);
+    table.appendChild(row);
+
     table.children[0].appendChild(row);
-    
+
+    document.getElementById('form').reset();
+
 
 };
